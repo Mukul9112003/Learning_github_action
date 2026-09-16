c1278effd86c70f0e07647bc93e0347db71fd6f2
Mukul Mehta <mukulmehta022@gmail.com>
2026-09-16 07:40:47 +0530

    day11 completed

---
+++ test/test_calculator.py
@@ -0,0 +1,7 @@
+from app.calculator import add, subtract, multiply
+def test_add():
+    assert add(2,5)==7
+def test_subtract():
+    assert subtract(7,2)==5
+def test_multiply():
+    assert multiply(2,5)==10
